08de9a10a43eda207c691b667d8d17835eb4a28d
Hooker Juggernaut <secretgspot@gmail.com>
2012-01-09 12:12:47 -0600

    missing

---
+++ js/classes/script.js
@@ -0,0 +1,29 @@
+var visible = false;
+document.onkeydown = console_slide;
+var Tilde = 192, Esc = 27;
+function console_slide(evt){
+  if (!evt) {
+    evt = window.event;
+  }
+  if (evt.keyCode === Esc || evt.keyCode === Tilde) {// Esc
+    if(visible==false){
+        visible = true;
+        $('#console').animate({
+            top: 0
+        });
+    }
+    else if(visible==true){
+        visible = false;
+        $('#console').animate({
+            top: -350
+        });
+    }
+    return false;
+  }
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// ON DOM READY
+////////////////////////////////////////////////////////////////////////////////
