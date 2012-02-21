dc20e89107e69bd4cbd89380b454bd09fa7cec02
Hooker Juggernaut <secretgspot@gmail.com>
2012-02-20 18:27:07 -0600

    cleain

--- app/modules/profile.js
+++ app/modules/profile.js
@@ -8,6 +8,23 @@
 	// This will fetch the tutorial template and render it.
 	Profile.Views.Home = Backbone.View.extend({
 		template: "app/templates/profile/main.html",
+
+		events: {
+			"click #profile-comments-toggle":    "toggleComments"
+		},
+		toggleComments: function(){
+			comments = $("#profile-comments-toggle");
+			if( comments.hasClass("active") ){
+				comments.removeClass("active");
+				comments.parent().find('.comment-body-wrap, .more').hide();
+				alert("Comments hidden");
+			} else{
+				comments.addClass("active");
+				comments.parent().find('.comment-body-wrap, .more').show();
+				alert("Displaying comments");
+			}
+		},
+
 		render: function(done) {
 			var view = this;
 			// Fetch the template, render it to the View element and call done.
