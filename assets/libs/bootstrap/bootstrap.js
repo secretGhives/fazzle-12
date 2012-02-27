bb0f3799b4ddf05cdcb906865b29f5ff770874ce
Hooker Juggernaut <secretgspot@gmail.com>
2012-02-27 03:17:53 -0600

    radio

--- assets/libs/bootstrap/bootstrap.js
+++ assets/libs/bootstrap/bootstrap.js
@@ -1731,3 +1731,39 @@
   })
 
 }( window.jQuery )
+
+
+/* Radio and toggle buttons */
+              
+!function( $ ) {
+
+    function activate( element, container ) {
+        container.find( '.active' ).removeClass( 'active' )
+        element.addClass('active')
+    }
+
+    jQuery.fn.radioGroup = function() {
+        
+        return this.each( function() {
+            var $this = $(this)
+            $this.click( function() {
+                activate( $this, $this.parent() )
+            })
+        })
+    }
+    
+    jQuery.fn.toggleBtn = function() {
+        return this.each( function() {
+            var $this = $(this)
+            $this.click( function(ev) {
+                var target = ev.originalEvent.target;
+                
+                // because the event bubble and is also triggered by the LABEL element
+                if (target.tagName != "INPUT") return;
+                
+                $this.toggleClass('active')
+            })
+        })
+    }
+    
+}( window.jQuery || window.ender );
