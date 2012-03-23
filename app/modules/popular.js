bbf2fcf0151d690ec080699156db62e450845837
Hooker Juggernaut <secretgspot@gmail.com>
2012-03-22 23:50:16 -0600

    mmm

---
+++ app/modules/popular.js
@@ -0,0 +1,8 @@
+(function($){
+
+FazzleX.Modules.register({
+	id: 'popular',
+	template: "app/templates/runway/popular.html"
+});
+
+})(jQuery);
---
+++ app/modules/top.js
@@ -0,0 +1,8 @@
+(function($){
+
+FazzleX.Modules.register({
+	id: 'top',
+	template: "app/templates/runway/top.html"
+});
+
+})(jQuery);
